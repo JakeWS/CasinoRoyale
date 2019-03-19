94c9d8c56915e1b99a286e4f91a2516569ab9f01
Jake Watts-Squires <40439387+JakeWS@users.noreply.github.com>
2019-03-19 09:17:59 -0700

    Began working on Roulette module
    Only has the class and main function currently implemented

---
+++ roulette.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <random>
+
+using namespace std;
+
+class RouletteTable{
+  private:
+  public:
+    RouletteTable();
+    ~RouletteTable();
+};
+
+int main()
+{
+  RouletteTable newRoulette;
+ 
+}
