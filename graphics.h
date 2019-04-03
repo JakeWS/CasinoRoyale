69924d71049a2244217b591f49b0877c42c97f40
Elizabeth Soechting <elizaso29@gmail.com>
2019-04-03 11:21:33 -0700

    First Commit
    Creation of a graphics header file

---
+++ graphics.h
@@ -0,0 +1,22 @@
+/***************************************
+ * Graphics header file
+ * Coded by Elizabeth Soechting
+ * for Casino Royale Project
+ * SE 310 & SE 420
+ ***************************************/
+
+ #include <iostream>
+
+ class Graphics
+ {
+   private:
+   public:
+      void setColor();
+      void setFont();
+      //void setSize();
+ };
+
+Graphics:: setFont()
+{
+  cout<<"Set Font Function"<<endl;
+}
