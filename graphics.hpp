1d10f6012b496f503097fe944796766fcc06d236
Elizabeth Soechting <elizaso29@gmail.com>
2019-04-04 10:09:26 -0700

    updates and comments from code walkthru

---
+++ graphics.hpp
@@ -0,0 +1,30 @@
+/***************************************
+ * Graphics header file
+ * Coded by Elizabeth Soechting
+ * for Casino Royale Project
+ * SE 310 & SE 420
+ ***************************************/
+#ifndef graphics_h
+#define graphics_h
+#include <iostream>
+#include <string.h>
+
+using namespace std;
+
+
+ class Graphics
+ {
+   private:
+   public:
+      void setColor();
+      void setFont();
+      //void setSize();
+ };
+
+void Graphics::setFont()
+{
+  cout<<"Set Font Function"<<endl;
+}
+
+
+#endif
