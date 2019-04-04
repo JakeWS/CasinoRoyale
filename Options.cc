bc02fd6250273b6fb1b30af9bb73d815cd8809cc
Elizabeth Soechting <elizaso29@gmail.com>
2019-04-04 10:10:19 -0700

    code walk through edits

--- Options.cc
+++ Options.cc
@@ -15,10 +15,16 @@ class Options
     int userFont;
     int userChoice;
     Options();  // constructor
-    //int setFont(); //uses graphics.hpp
-    //int setColor();  // uses graphics.hpp
+    //int setFont(); // in graphics.hpp
+    //int setColor();  // in graphics.hpp
 };
+// test driver
 
+// # defines for "magic numbers"
+// use enumeration and implement switch and case instead of if else
+  // change the numbers ionto # defines
+
+  //
 
 int main()
 {
@@ -34,71 +40,71 @@ Options::Options()
 
   while(continueLoop)
   {
-    cout<<"What would you like to do?"<<endl<<"0. Exit Options."<<endl<<"1. Change Font."<<endl<<"2. Change Font Color."<<endl;
-    cin>> userAction;
+    std::cout<<"What would you like to do?"<<std::endl<<"0. Exit Options."<<std::endl<<"1. Change Font."<<std::endl<<"2. Change Font Color."<<std::endl;
+    std::cin>> userAction;
 
     if(userAction == 0)
     {
-      cout<<"Exiting Options Menu...."<<endl;
+      std::cout<<"Exiting Options Menu...."<<std::endl;
       continueLoop = false;
     }
     else if(userAction == 1)
     {
-      cout<<"Which of the following fonts would you like to use?"<<endl<<"0.I do not want to change my font."<<endl<<"1. Font1"<<endl<<"2. Font2"<<endl;
-      cin>>userFont;
+      std::cout<<"Which of the following fonts would you like to use?"<<std::endl<<"0.I do not want to change my font."<<std::endl<<"1. Font1"<<std::endl<<"2. Font2"<<std::endl;
+      std::cin>>userFont;
 
       //if else block for each of the fonts
       if(userFont == 0)
       {
-        cout<<"Keeping Font Choice unaltered."<<endl;
+        std::cout<<"Keeping Font Choice unaltered."<<std::endl;
         //continueLoop = false
       }
       else if(userFont == 1)
       {
-        cout<<"Setting font to desired font choice..."<<endl;
+        std::cout<<"Setting font to desired font choice..."<<std::endl;
         //set font function
         setFont(1);
       }
       else if(userFont == 2)
       {
-        cout<<"Setting font to desired font choice..."<<endl;
+        std::cout<<"Setting font to desired font choice..."<<std::endl;
         //set font function
         setFont(2);
       }
       else
       {
-        cout<<"Unable to process, please respond to the promp again."<<endl;
+        std::cout<<"Unable to process, please respond to the promp again."<<std::endl;
       }
     }
     else if(userAction == 2)
     {
-      cout<<"Which of the following font colors would you like to implement?"<<endl<<"0. No color change."<<endl<<"1. Color1"<<endl<<"2. Color2"<<endl;
-      cin>>userChoice;
+      std::cout<<"Which of the following font colors would you like to implement?"<<std::endl<<"0. No color change."<<std::endl<<"1. Color1"<<std::endl<<"2. Color2"<<std::endl;
+      std::cin>>userChoice; 
 
       //if else block for color choices
       if(userChoice == 0)
       {
-        cout<<"Keeping font color unaltered..."<<endl;
+        std::cout<<"Keeping font color unaltered..."<<std::endl;
 
       }
       else if(userChoice == 1)  // Color 1
       {
-        cout<<"Setting font color to desired color..."<<endl;
+        std::cout<<"Setting font color to desired color..."<<std::endl;
         //set color function
       }
       else if(userChoice == 2)  // Color2
       {
-        cout<<"Setting font color to desired color..."<<endl;
+        std::cout<<"Setting font color to desired color..."<<std::endl;
         //set color function
       }
       else
       {
-        cout<<"Unable to process, please respond to the promp again."<<endl;
+        std::cout<<"Unable to process, please respond to the promp again."<<std::endl;
       }
     }
     else
     {
-      cout<<"Unable to process, please respond to the promp again."<<endl;
+      std::cout<<"Unable to process, please respond to the promp again."<<std::endl;
     }
   }
 
