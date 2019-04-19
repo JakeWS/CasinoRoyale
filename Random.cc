054c0904c756364f3e6c70c0918a63330369c402
Elizabeth Soechting <47231447+ElizabethSoechting@users.noreply.github.com>
2019-04-19 12:29:58 -0700

    adding file
    Adding file to branch

---
+++ Random.cc
@@ -0,0 +1,27 @@
+// Coded by Matthew J.
+
+#include <chrono>
+#include <random>
+#include "randGen.h"
+
+using namespace std;
+
+double randGen::randomDoubleGenerator()
+{
+	double random;
+	unsigned int seed = chrono::system_clock::now().time_since_epoch().count();
+	default_random_engine generator(seed);
+	uniform_real_distribution<double> distribution(0,9);
+	random = distribution(generator);
+	return random;
+}
+
+int randGen::randomIntGenerator(int i)
+{
+	unsigned int random;
+	unsigned int seed = chrono::system_clock::now().time_since_epoch().count();
+	default_random_engine generator(seed);
+	uniform_real_distribution<double> distribution(0,9);
+	random = ((unsigned int)(distribution(generator)*1000000))%i;
+	return random;
+}
