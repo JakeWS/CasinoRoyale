0f819bb0fa7aaea3508864d503ccda30dd2228f8
Elizabeth Soechting <47231447+ElizabethSoechting@users.noreply.github.com>
2019-04-19 12:29:11 -0700

    cerate randgen
    adding file to branch

---
+++ randGen.h
@@ -0,0 +1,10 @@
+// Coded by Matthew J.
+
+#ifndef randGen_h
+#define randGen_h
+class randGen{
+	public:
+		static double randomDoubleGenerator(void);
+		static int randomIntGenerator(int i);
+};
+#endif
